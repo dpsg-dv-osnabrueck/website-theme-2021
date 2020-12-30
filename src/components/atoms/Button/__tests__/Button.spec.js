96f48fd794059678a7ba64b48b95f60707abcca7
Dominik Niemann <dominikniemann@posteo.de>
2020-12-30 09:43:45 +0100

    Add unit test for button component

---
+++ src/components/atoms/Button/__tests__/Button.spec.js
@@ -0,0 +1,54 @@
+import Button from '@/components/atoms/Button/Button.vue';
+import Icon from '@/components/atoms/Icon/Icon.vue';
+import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+
+describe('Button test', () => {
+  let wrapper;
+  beforeEach(() => {
+    Vue.component('Icon', Icon);
+    wrapper = mount(Button, {
+      slots: {
+        default: 'Button Label',
+      },
+    });
+  });
+
+  test('Button has label', () => {
+    expect(wrapper.html()).toContain('<span>Button Label</span>');
+    expect(wrapper.find('button').element.disabled).toBe(false);
+  });
+
+  test('Button has icon left', async () => {
+    wrapper.setProps({ iconName: 'chevron-right' });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.html()).toContain('<span class="icon"><i class="fas fa-chevron-right"></i></span> <span>Button Label</span>');
+  });
+
+  test('Button has branded icon', async () => {
+    wrapper.setProps({ iconName: 'instagram', fab: true });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.html()).toContain('<span class="icon"><i class="fab fa-instagram"></i></span> <span>Button Label</span>');
+  });
+
+  test('Button has icon right', async () => {
+    wrapper.setProps({ iconName: 'chevron-right', iconRight: true });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.html()).toContain('<span>Button Label</span> <span class="icon"><i class="fas fa-chevron-right"></i></span>');
+  });
+  test('Button test props with classes', () => {
+    const { classNames } = wrapper.vm.$data;
+    Object.keys(classNames).forEach(async (key) => {
+      wrapper.setProps({ [key]: true });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.attributes().class).toContain(classNames[key]);
+      expect(true).toBe(true);
+    });
+  });
+
+  test('Button is disabled', async () => {
+    wrapper.setProps({ disabled: true });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('button').element.disabled).toBe(true);
+  });
+});
